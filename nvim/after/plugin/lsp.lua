bccfe1bfb642770933b247f4bddd70f6d252c8e0
Schlypar <schlypar@gmail.com>
2023-07-27 14:51:46 +0300

    initial nvim

---
+++ nvim/after/plugin/lsp.lua
@@ -0,0 +1,99 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	'clangd',
+	'rust_analyzer',
+    'pyright',
+})
+
+local mason = require("mason")
+
+mason.ensure_installed = {
+    -- 'mypy',
+    -- 'ruff',
+    'black',
+    'debugpy',
+}
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<enter>'] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+-- cmp_mappings['<Tab>'] = nil
+-- cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = 'E',
+		warn = 'W',
+		hint = 'H',
+		info = 'I'
+	}
+})
+
+lsp.on_attach(function(client, bufnr)
+	local opts = {buffer = bufnr, remap = false}
+    lsp.default_keymaps({buffer = bufnr})
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+vim.diagnostic.config({
+	virtual_text = true
+})
+
+local null_ls = require('null-ls')
+local formatting = null_ls.builtins.formatting
+local lint  = null_ls.builtins.diagnostics
+
+null_ls.setup({
+    sources = {
+        formatting.clang_format,
+        lint.clang_check,
+
+        formatting.black,
+        -- lint.mypy,
+        lint.ruff,
+    },
+})
+
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    -- ['lua_ls'] = {'lua'},
+    -- ['rust_analyzer'] = {'rust'},
+    -- if you have a working setup with null-ls
+    -- you can specify filetypes it can format.
+    ['null-ls'] = {'cpp', 'c', 'rust', 'python'},
+  }
+})
+
+lsp.setup()
